6841ca2984a89c726be930804bbaf149eb3c7f86
rojiloro <kelengoji79@gmail.com>
2023-05-21 14:59:45 +0700

    add file js

---
+++ js/project.js
@@ -0,0 +1,17 @@
+// post project
+let dataProject = [];
+
+function addProject(event) {
+  event.preventDefault();
+
+  let nama = document.getElementById("input-project-name").value;
+  let tanggal = document.getElementById("input-date").value;
+  let deskripsi = document.getElementById("description").value;
+  let nodejs = document.getElementById("nodejs").value;
+  let nextjs = document.getElementById("nextjs").value;
+  let reactjs = document.getElementById("reactjs").value;
+  let typescript = document.getElementById("typescript").value;
+  let image = document.getElementById("input-blog-image").files;
+
+  
+}
